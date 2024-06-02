4cbe0fad2aa0823890d96b05f2a80215bc701b3d
Aditya030an <adityaanu20023@gmail.com>
2024-06-03 00:58:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <p className='border-2 border-red-500 border-solid'>sfsnf skdvnos vsvosbv</p>
+    </div>
+  )
+}
+
+export default App
